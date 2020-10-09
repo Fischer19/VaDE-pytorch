d4b4ebbb570bc0371c1f8ab15cc7893550a13a64
jiz077 <jiz077@jiz077-21768.pods.jiz077.svc.cluster.local>
2020-10-08 19:39:40 -0700

    update compute objective

--- compute_objective.py
+++ compute_objective.py
@@ -61,7 +61,7 @@ vade.pre_train(DL,pre_epoch=50)
 mean, _ = vade.encoder(X)
 mean = mean.detach().numpy()
 
-transformed_mean = transformation(vade, X)
+transformed_mean = transformation(vade, X.numpy())
 
 Z = linkage(y[:2000].reshape(-1,1), "ward")
 rootnode, nodelist = scipy.cluster.hierarchy.to_tree(Z, rd=True)
